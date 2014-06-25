b12919fae29ac6bfba5072a9ee960ba8bf00bbbc
mbedward <michael.bedward@gmail.com>
2014-06-25 13:08:52 +1000

    Added -p command line flag to turn on echo of input parameters in the output (default is off)

--- forest/test.cc
+++ forest/test.cc
@@ -16,7 +16,7 @@ using std::vector;
 using std::cout;
 using std::endl;
 
-void process(std::string inPath, std::ostream &outputStream) {
+void process(std::string inPath, std::ostream &outputStream, bool paramsFlag) {
 
   std::pair<Results::OutputLevelType, int> waddaYaWant = prelimParseInputTextFile(inPath);
   Results::OutputLevelType outputLevel = waddaYaWant.first;
@@ -26,7 +26,10 @@ void process(std::string inPath, std::ostream &outputStream) {
   
   if (!monteCarlo) {
     Location loc = parseInputTextFile(inPath, monteCarlo);
-    outputStream << loc.printToString() << endl;
+
+    if (paramsFlag) 
+      outputStream << loc.printToString() << endl;
+    
     Results res = loc.results();
 
     outputStream << res.printToString(outputLevel) << endl;
@@ -55,25 +58,45 @@ void process(std::string inPath, std::ostream &outputStream) {
 }
 
 int main(int argc, char *argv[]) {
+  std::string usage("usage: ffm input_file [output_file] [-p]"); 
+
   if (argc < 2) {
-    cout << "usage: ffm input_file [output_file]" << endl;
+    cout << usage << endl;
     return 0;
   }
 
-  std::string inPath = std::string(argv[1]);
+  // collect the inPath and the optional outPath and flag arguments
+  bool paramsFlag;
+  std::string flag_str("-p");
+  std::string inPath;
+  std::string outPath;
 
-  // optional output path - if not provided output is sent to console
-  bool isFileOutput = false;
+  for (int i = 1; i < argc; i++) {
+    std::string arg( argv[i] );
+
+    if (arg.compare(0, flag_str.size(), flag_str) == 0)
+      paramsFlag = true;
+    else if (inPath.empty())
+      inPath = arg;
+    else if (outPath.empty())
+      outPath = arg;
+  }
+
+  if (inPath.empty()) {
+    cout << usage << endl;
+    return 0;
+  }
+
   std::ostream* fp = &cout;
   std::ofstream fout;
-  std::string outPath = argc >= 3 ? std::string(argv[2]) : "";
   if( !outPath.empty()) {
     fout.open(outPath); 
     fp = &fout;
-    isFileOutput = true;
   }
 
-  process(inPath, *fp);
+  process(inPath, *fp, paramsFlag);
   if (!outPath.empty()) fout.close();
+
+  return 0;
 }
 
