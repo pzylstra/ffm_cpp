22fade5aaa1b635346f75e41141e3440fe5833dc
mbedward <michael.bedward@gmail.com>
2016-03-23 14:51:09 +1100

    Added -d command line flag. When this is supplied, and it is a monte-carlo run, the program writes the current generated parameters to the file debug_mc_params.csv before processing that iteration.

--- forest/test.cc
+++ forest/test.cc
@@ -19,7 +19,7 @@ using std::set;
 using std::cout;
 using std::endl;
 
-void process(std::string inPath, std::ostream &outputStream, bool paramsFlag) {
+void process(std::string inPath, std::ostream &outputStream, bool paramsFlag, bool debugFlag) {
 
   std::pair<Results::OutputLevelType, int> prelimPars = prelimParseInputTextFile(inPath);
 
@@ -48,6 +48,14 @@ void process(std::string inPath, std::ostream &outputStream, bool paramsFlag) {
         Results res = loc.results();
 
         outputStream << printMonteCarloInputs(loc);
+
+        if (debugFlag) {
+          std::ofstream debugstrm("debug_mc_params.csv", std::ofstream::out);
+          debugstrm << printMonteCarloHeader(loc);
+          debugstrm << printMonteCarloInputs(loc);
+          debugstrm.close();
+        }
+
         outputStream << printMonteCarloResults(res) << endl;
 
         i++ ;
@@ -58,7 +66,7 @@ void process(std::string inPath, std::ostream &outputStream, bool paramsFlag) {
 }
 
 int main(int argc, char *argv[]) {
-  std::string usage("usage: ffm input_file [output_file] [-p]"); 
+  std::string usage("usage: ffm input_file [output_file] [-p] [-d]"); 
 
   if (argc < 2) {
     cout << usage << endl;
@@ -66,16 +74,20 @@ int main(int argc, char *argv[]) {
   }
 
   // collect the inPath and the optional outPath and flag arguments
-  bool paramsFlag;
-  std::string flag_str("-p");
+  bool paramsFlag = false;
+  bool debugFlag = false;
+  std::string params_flag_str("-p");
+  std::string debug_flag_str("-d");
   std::string inPath;
   std::string outPath;
 
   for (int i = 1; i < argc; i++) {
     std::string arg( argv[i] );
 
-    if (arg.compare(0, flag_str.size(), flag_str) == 0)
+    if (arg.compare(0, params_flag_str.size(), params_flag_str) == 0)
       paramsFlag = true;
+    else if (arg.compare(0, debug_flag_str.size(), debug_flag_str) == 0)
+      debugFlag = true;
     else if (inPath.empty())
       inPath = arg;
     else if (outPath.empty())
@@ -94,7 +106,7 @@ int main(int argc, char *argv[]) {
     fp = &fout;
   }
 
-  process(inPath, *fp, paramsFlag);
+  process(inPath, *fp, paramsFlag, debugFlag);
   if (!outPath.empty()) fout.close();
 
   return 0;
